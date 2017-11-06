6550778c46f56198e26b09c45848b9d13226d189
zengrui <820318767@qq.com>
2017-11-06 19:35:03 +0800

    添加配置中心文件

--- ranran-config-client/src/main/java/com/ranran/ConfigClientApp.java
+++ ranran-config-client/src/main/java/com/ranran/ConfigClientApp.java
@@ -1,12 +1,26 @@
 package com.ranran;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Hello world!
  *
  */
+@SpringBootApplication
+@RestController
 public class ConfigClientApp {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        SpringApplication.run(ConfigClientApp.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
     }
 }
